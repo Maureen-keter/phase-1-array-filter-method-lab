b1471a9e47c0c623226e655ff9e8b573c7536ba0
maureen <maureen.chelangat@student.moringaschool.com>
2023-10-03 15:40:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// return all drivers that match passed name
+function findMatching(drivers, name) {
+    const lowercaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+  }
+//   returns driver if beginning provided letters match
+  function fuzzyMatch(drivers, name) {
+    const lowercaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowercaseName));
+  }
+//   accesses data structure to check if name matches
+  function matchName(drivers,name){
+    return drivers.filter(driver=>driver.name===name);
+  }
